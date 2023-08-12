f4459983def9ce504b54045079419f42e69ecd7e
Lrivelt17 <141666807+Lrivelt17@users.noreply.github.com>
2023-08-12 16:13:57 -0400

    Boutik.sql

--- person.sql
+++ person.sql
@@ -3,3 +3,5 @@ Create table Pwodui ( non varchar (30) not null, deskripsyon text not null, pri 
 
 Alter table Pwodui  change non name varchar (40);
 
+Alter table Pwodui  change deskripsyon description text;
+Alter table Pwodui  change pri price int;
