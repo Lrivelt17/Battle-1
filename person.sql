b8a15ae10891b6809e5c2299069f6bc2e5e8256d
Lrivelt17 <141666807+Lrivelt17@users.noreply.github.com>
2023-08-12 22:53:49 -0400

    Delete person.sql

--- person.sql
+++
@@ -1,7 +0,0 @@
-Create database boutik character set 'utf8mb4';
-Create table Pwodui ( non varchar (30) not null, deskripsyon text not null, pri int);
-
-Alter table Pwodui  change non name varchar (40);
-
-Alter table Pwodui  change deskripsyon description text;
-Alter table Pwodui  change pri price int;
